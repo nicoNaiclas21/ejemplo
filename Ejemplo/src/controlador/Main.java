557de7fb91f5ffc661824bd6a240eaf54f134637
ikasle <ikasle@DAM1-07.arangoya.local>
2025-03-25 14:13:40 +0100

    Version: 1.0.5

--- Ejemplo/src/controlador/Main.java
+++ Ejemplo/src/controlador/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo, nico gay. ALFRED intento 2 ");
+		System.out.println("Hola mundo, nico gay. ALFRED intento 3 ");
 	}
 
 }
